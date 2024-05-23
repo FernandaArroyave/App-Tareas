ef0346173963b6c21e5642915216bc6fe0eed8e1
Fernanda Arroyave <arroyavefernanda97@gmail.com>
2024-05-23 17:58:32 -0500

    manejo de formularios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Task } from './../../models/task.model';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
+    tasks = signal<Task[]>([ /*Crear una lista*/ 
+      {
+        id: Date.now(),
+        title: 'Crear proyecto',
+        completed: false
+      },
+      {
+        id: Date.now(),
+        title: 'Crear componentes',
+        completed: false
+      },
+      
+    ]);
+
+newTaskCtrl = new FormControl('',{
+  nonNullable: true,
+  validators:[
+    Validators.required,
+  ]
+});
+
+changeHandler() {
+  if(this.newTaskCtrl.valid){
+    const value = this.newTaskCtrl.value.trim();
+  this.addTask(value);
+  this.newTaskCtrl.setValue('');
+  }
+}
+
+addTask(title: string) {
+  if(title == "" ){
+  return
+  }
+  
+  const newTasks = {
+    id: Date.now(),
+    title,
+    completed: false,
+  };
+  this.tasks.update((tasks)=> [...tasks, newTasks]) /* update permite modificar o agregar un nuevo valor 
+                                                    pero no borrarlo o recetearlo desde cero  */
+};
+deleteTask(index:number){ /*Genera un filtro en las tareas, con base al estado anterior
+                          el array con todas la tareas estamos filtrando y dejando por 
+                          fuera de ese array la posicion por ende se crea un nuevo estado
+                          sin esa tarea es decir se elimina*/
+  this.tasks.update((tasks)=> tasks.filter((task, position) => position !== index));
+};
+updateTask (index: number){ /*Algoritmo basado en una posicion en especifico y utilizando el patron o el paradigama de no mutar el array  */
+  this.tasks.update((tasks) =>{
+    return tasks.map((task, position) =>{
+      if (position === index) {
+        return{
+          ...task,
+          completed: !task.completed
+        }
+      }
+      return task;
+    })
+  })
+}
 
 }
+
+
+
+
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,39 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
     Welcome = 'Hola!';
-    tasks = [ /*Crear una lista*/ 
+    tasks = signal([ /*Crear una lista*/ 
       'Instalaa el Angular CLI',
       'Crear Proyecto',
       'Crear componentes'
-    ];
-    name = 'Vanessa';
+    ]);
+    name = signal ('Vanessa'); /* se asignan señales "signal" para que tengan reactividad */
     age = 20;
     disabled = true;
     img = 'https://th.bing.com/th/id/R.b91fd66206d4f502d454fb7496df22c5?rik=S7zpQzWO4TB5Wg&pid=ImgRaw&r=0';
 
-    person = {
-      name: 'Vanessa',
-      age: 20,
+    person = signal( {
+      name: 'vanessa',
+      age: 5,
       avatar:'https://th.bing.com/th/id/R.b91fd66206d4f502d454fb7496df22c5?rik=S7zpQzWO4TB5Wg&pid=ImgRaw&r=0'
+    });
+
+    colorCtrl = new FormControl();
+    constructor(){
+      this.colorCtrl.valueChanges.subscribe(value =>{
+        console.log(value);
+      })
     }
+
     clickHandler(){ /*Metodo de la clase*/
       alert('Hola')
     }
-      changeHandler(event: Event){
-        console.log(event)
+      changeHandler(event: Event){ /*Hace que cambie el valor de forma reactiva*/
+        const input = event.target as HTMLInputElement;
+        const newValue = input.value;
+        this.name.set(newValue)
       }
-      keydownHandler(event:KeyboardEvent){
+      keydownHandler(event:KeyboardEvent){ /*Se recibe Detalle de que tecla se presiono, quien la presiono*/
         const input = event.target as HTMLInputElement;
         console.log(input.value);
       }
+
+      changeAge(event: Event){ /*Hace que cambie el valor de forma reactiva con un objeto */
+      const input = event.target as HTMLInputElement;
+      const newValue = input.value;
+      this.person.update(prevState => {
+        return{
+          ...prevState,
+          age: parseInt(newValue, 10)
+        }
+      });
     }
+
+    
+    changeName(event: Event){ /*Hace que cambie el valor de forma reactiva con un objeto */
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState => {
+      return{
+        ...prevState,
+        name: newValue
+      }
+      })
+    };
+  }
+  
 
